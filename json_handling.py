fb5e41cc13986f4470fa4c7c98c8edde4e8e5970
Sofia Briquet <sbriquet@bu.edu>
2021-03-30 11:10:14 -0400

    added file with helper functios for the optimization approach

---
+++ json_handling.py
@@ -0,0 +1,84 @@
+import json
+import os
+
+
+# file with helper functions to run main script
+
+
+# function to open and extract data from json file (input)
+def open_json(in_dir, file_name):
+    input_file = os.path.join(in_dir, file_name)
+    file = open(input_file, "r")
+    data = json.load(file)
+    file.close()
+    return data
+
+
+# function to write modified input json file with optimized parameters
+def output_json(data, in_dir, new_file_name):
+    new_file = json.dumps(data, indent=4)
+    complete_name = os.path.join(in_dir, new_file_name)
+    f = open(complete_name, "w")
+    f.write(new_file)
+    f.close()
+
+
+# function to extract parameters from  best 2 input signals
+def extract_parameters(data, best_input_signals):
+    signal_one = best_input_signals[0]
+    signal_two = best_input_signals[1]
+    # check for signal one
+    if signal_one == 'LacI':
+        parameters_one = data[1].get("parameters")
+    elif signal_one == 'TetR':
+        parameters_one = data[4].get("parameters")
+    elif signal_one == "AraC":
+        parameters_one = data[7].get("parameters")
+    elif signal_one == 'LuxR':
+        parameters_one = data[10].get("parameters")
+    # extract ymax, ymin, alpha and beta parameters
+    ymax_one = parameters_one[0].get('value')
+    ymin_one = parameters_one[1].get('value')
+    alpha_one = parameters_one[2].get('value')
+    beta_one = parameters_one[3].get('value')
+    # check for signal two
+    if signal_two == 'LacI':
+        parameters_two = data[1].get("parameters")
+    elif signal_two == 'TetR':
+        parameters_two = data[4].get("parameters")
+    elif signal_two == "AraC":
+        parameters_two = data[7].get("parameters")
+    elif signal_two == 'LuxR':
+        parameters_two = data[10].get("parameters")
+    # extract ymax, ymin, alpha and beta parameters
+    ymax_two = parameters_two[0].get('value')
+    ymin_two = parameters_two[1].get('value')
+    alpha_two = parameters_two[2].get('value')
+    beta_two = parameters_two[3].get('value')
+    return ymax_one, ymin_one, alpha_one, beta_one, ymax_two, ymin_two, alpha_two, beta_two
+
+
+# function to modify parameters
+def modify_parameters_single_signal(data, signal_name, new_ymax, new_ymin, new_alpha, new_beta):
+    if signal_name == 'LacI':
+        data[1]['parameters'][0]['value'] = new_ymax
+        data[1]['parameters'][1]['value'] = new_ymin
+        data[1]['parameters'][2]['value'] = new_alpha
+        data[1]['parameters'][3]['value'] = new_beta
+    elif signal_name == 'TetR':
+        data[4]['parameters'][0]['value'] = new_ymax
+        data[4]['parameters'][1]['value'] = new_ymin
+        data[4]['parameters'][2]['value'] = new_alpha
+        data[4]['parameters'][3]['value'] = new_beta
+    elif signal_name == 'AraC':
+        data[7]['parameters'][0]['value'] = new_ymax
+        data[7]['parameters'][1]['value'] = new_ymin
+        data[7]['parameters'][2]['value'] = new_alpha
+        data[7]['parameters'][3]['value'] = new_beta
+    elif signal_name == 'LuxR':
+        data[10]['parameters'][0]['value'] = new_ymax
+        data[10]['parameters'][1]['value'] = new_ymin
+        data[10]['parameters'][2]['value'] = new_alpha
+        data[10]['parameters'][3]['value'] = new_beta
+    return data
+
--- main.py
+++ main.py
@@ -1,123 +1,37 @@
 import json
 import os
+from json_handling import *
 from celloapi2 import CelloQuery, CelloResult
+from itertools import combinations
 
 # ASK USER TO PROVIDE FILE PATH OF INPUT AND OUTPUT DIRECTORIES AS WELL AS DESIRED GATE
 from click._compat import raw_input
 
-prompt = "Plase enter the path of your input directory containing: input.json file, output.json file," \
+prompt = "Please enter the path of your input directory containing: input.json file, output.json file," \
          "UCF.json file and verilog files (boolean gates): "
 in_dir = raw_input(prompt)
-assert os.path.exists(in_dir), "I did not find directory name at, " +str(in_dir)
+assert os.path.exists(in_dir), "I did not find directory name at, " + str(in_dir)
 prompt2 = "Please enter path for where you want to save results: "
 out_dir = input(prompt2)
-assert os.path.exists(out_dir), "I did not find directory name at, " +str(out_dir)
+assert os.path.exists(out_dir), "I did not find directory name at, " + str(out_dir)
 v_file = input("Please state which gate (eg: 'and.v') file you want to use: ")
 options = 'options.csv'
 
-#define chassis to evaluate
+# define chassis to evaluate
 chassis_name = input("which chassis do you want to evaluate: ")
+input_sensor_file = f'/{chassis_name}.input.json'
 # read input json file
-input_file = in_dir + f'/{chassis_name}.input.json'
-file = open(input_file,)
-# load json file as a list
-data = json.load(file)
-#check the data structure loaded correctly
-#print(data)
-#close file
-file.close()
-
-#split list
-# LacI
-LacI_parameters = data[1].get("parameters")
-ymax_lac = LacI_parameters[0].get('value')
-ymin_lac = LacI_parameters[1].get('value')
-alpha_lac = LacI_parameters[2].get('value')
-beta_lac = LacI_parameters[3].get('value')
-
-# TetR
-TetR_parameters = data[4].get("parameters")
-ymax_tet = TetR_parameters[0].get('value')
-ymin_tet = TetR_parameters[1].get('value')
-alpha_tet = TetR_parameters[2].get('value')
-beta_tet = TetR_parameters[3].get('value')
-
-# AraC
-AraC_parameters = data[7].get("parameters")
-ymax_ara = AraC_parameters[0].get('value')
-ymin_ara = AraC_parameters[1].get('value')
-alpha_ara = AraC_parameters[2].get('value')
-beta_ara = AraC_parameters[3].get('value')
-
-# LuxR
-LuxR_parameters = data[10].get("parameters")
-ymax_lux = LuxR_parameters[0].get('value')
-ymin_lux = LuxR_parameters[1].get('value')
-alpha_lux = LuxR_parameters[2].get('value')
-beta_lux = LuxR_parameters[3].get('value')
+data = open_json(in_dir, input_sensor_file)
 
 # NUMERICAL OPTIMIZATION
-#intialize parameters for now (there should really be the results from optimization algorithm)
-# LacI
-new_ymax_lac = 0.1
-new_ymin_lac = 0.1
-new_alpha_lac = 0.1
-new_beta_lac = 0.1
-#TetR
-new_ymax_tet = 0.1
-new_ymin_tet = 0.1
-new_alpha_tet = 0.1
-new_beta_tet = 0.1
-# AraC
-new_ymax_ara = 0.1
-new_ymin_ara = 0.1
-new_alpha_ara = 0.1
-new_beta_ara = 0.1
-# LuxR
-new_ymax_lux = 0.1
-new_ymin_lux = 0.1
-new_alpha_lux = 0.1
-new_beta_lux = 0.1
-
-# WRITE AND SAVE MODIFIED INPUT JSON FILE
-#new_file = data
-# Lac I
-data[1]['parameters'][0]['value'] = new_ymax_lac
-data[1]['parameters'][1]['value'] = new_ymin_lac
-data[1]['parameters'][2]['value'] = new_alpha_lac
-data[1]['parameters'][3]['value'] = new_beta_lac
-# Tet R
-data[4]['parameters'][0]['value'] = new_ymax_tet
-data[4]['parameters'][1]['value'] = new_ymin_tet
-data[4]['parameters'][2]['value'] = new_alpha_tet
-data[4]['parameters'][3]['value'] = new_beta_tet
-# Ara C
-data[7]['parameters'][0]['value'] = new_ymax_ara
-data[7]['parameters'][1]['value'] = new_ymin_ara
-data[7]['parameters'][2]['value'] = new_alpha_ara
-data[7]['parameters'][3]['value'] = new_beta_ara
-# Lux R
-data[10]['parameters'][0]['value'] = new_ymax_lux
-data[10]['parameters'][1]['value'] = new_ymin_lux
-data[10]['parameters'][2]['value'] = new_alpha_lux
-data[10]['parameters'][3]['value'] = new_beta_lux
-
-#covert file to json
-new_file = json.dumps(data, indent= 4)
-# save file to directory
-output_sensor_file = f'new{chassis_name}.input.json'
-#add the file to the input directory specified by the user
-completeName = os.path.join(in_dir, output_sensor_file)
-f = open(completeName, 'w')
-#write content
-f.write(new_file)
-#close file
-f.close()
-
-#SUBMIT ORIGINAL INPUT FILE
+# SUBMIT ORIGINAL INPUT FILE
 output_device_file = f'{chassis_name}.output.json'
-input_sensor_file = f'{chassis_name}.input.json'
 in_ucf = f'{chassis_name}.UCF.json'
+# number of inputs into the circuit:
+signal_input = 2
+# define best circuit score from best 2 input signals (out of the 4 available)
+best_score = 0
+best_input_signals = None
 q = CelloQuery(
     input_directory=in_dir,
     output_directory=out_dir,
@@ -126,16 +40,79 @@ q = CelloQuery(
     input_ucf=in_ucf,
     input_sensors=input_sensor_file,
     output_device=output_device_file,
+    # logging=True,
 )
-#OBTAIN RESULTS FROM CELLO
-# Submit our query to Cello. This might take a second.
-q.get_results()
-# Fetch our Results.
-res2 = CelloResult(results_dir=out_dir)
+signals = q.get_input_signals()
+signal_pairing = list(combinations(signals, signal_input))
+for signal_set in signal_pairing:
+    signal_set = list(signal_set)
+    q.set_input_signals(signal_set)
+    # OBTAIN RESULTS FROM CELLO
+    q.get_results()
+    # Fetch our Results.
+    try:
+        res2 = CelloResult(results_dir=out_dir)
+        if res2.circuit_score > best_score:
+            best_score = res2.circuit_score
+            best_input_signals = signal_set
+    except:
+        pass
+    q.reset_input_signals()
 print("The score of the original circuit is:")
-print(res2.circuit_score)
+print(best_score)
+print(f'Best input signals: {best_input_signals} ')
 
-# SUBMIT MODIFIED INPUT FILE TO CELLO
+# OPTIMIZE CIRCUIT WITH THE BEST INPUT SIGNALS FOUND
+# extract parameters from best input signals found
+[ymax_one, ymin_one, alpha_one, beta_one, ymax_two, ymin_two, alpha_two, beta_two] = extract_parameters(data, best_input_signals)
+
+
+# first try DNA engineering operations
+# 1) STRONGER/ WEAKER PROMOTER (depending on x value)
+def change_promoter(ymax, ymin, x):
+    new_ymax = ymax * x
+    new_ymin = ymin * x
+    return new_ymax, new_ymin
+
+
+# 2) STRONGER/WEAKER RBS (depending on x value)
+def change_rbs(beta, x):
+    new_beta = beta / x
+    return new_beta
+
+
+# try powers of 10 for x (to make weaker/stronger promoter and RBS)
+x_values = [0.0001, 0.001, 0.01, 0.1, 1, 10, 100, 1000]
+# TRY NUMERICAL OPTIMIZATION HERE
+
+# if score doesn't improve significantly, use protein-engineering operations
+# 3) STRETCH --> x can be at most 1.5
+def stretch(ymax, ymin, x):
+    new_ymax = ymax * x
+    new_ymin = ymin / x
+    return new_ymax, new_ymin
+
+
+# 4) INCREASE SLOPE --> x can be at most 1.05
+def increase_slope(alpha, x):
+    new_alpha = alpha * x
+    return new_alpha
+
+
+# 7) DECREASE SLOPE --> x can be at most 1.05
+def decrease_slope(alpha, x):
+    new_alpha = alpha / x
+    return new_alpha
+
+
+# WRITE AND SAVE MODIFIED INPUT JSON FILE
+# call modify_parameters_single_signal for both signals
+
+# convert new file to json and save to input directory
+new_input_file = f'new{chassis_name}.input.json'
+output_json(data, in_dir, new_input_file)
+
+# Submit modified input file to Cello
 # Set up input files.
 in_ucf = f'{chassis_name}.UCF.json'
 input_sensor_file = f'new{chassis_name}.input.json'
@@ -151,10 +128,13 @@ q = CelloQuery(
 )
 
 # Submit our query to Cello. This might take a second.
+# set inout signals to be the best two found before
+q.set_input_signals(best_input_signals)
 q.get_results()
 # Fetch our Results.
 res = CelloResult(results_dir=out_dir)
 print(res.circuit_score)
 
-delta = res.circuit_score - res2.circuit_score
+# print how much the circuit score improved after optimization
+delta = res.circuit_score - best_score
 print("Circuit improved by", delta)
